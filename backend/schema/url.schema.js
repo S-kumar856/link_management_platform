fc9075b103fccc018df3f5128e5821271e57b7ac
S KUMAR <hk856856@gmail.com>
2025-01-24 14:54:52 +0530

    modify schema

--- backend/schema/url.schema.js
+++ backend/schema/url.schema.js
@@ -4,36 +4,36 @@ const urlSchema = new mongoose.Schema({
     destinationUrl: {
         type: String,
         required: true,
-        trim: true
+    
     },
     remarks: {
         type: String,
         // required: true,
-        trim: true,
+       
     },
-    linkExpiration: {
-        enabled: {
-            type: Boolean,
-            // required: true,
-            default: false,
-        },
-        expiryDate: {
-            type: Date,
-            validate: {
-                validator: function (value) {
-                    // expirationDate is required only if enabled is true
-                    return this.linkExpiration.enabled ? value != null : true;
-                },
-                message:
-                    "Expiration date is required when link expiration is enabled.",
-            },
-        },
-    },
+    expiryDate:{ type:date },
+    // linkExpiration: {
+    //     enabled: {
+    //         type: Boolean,
+    //         // required: true,
+    //         default: false,
+    //     },
+    //     expiryDate: {
+    //         type: Date,
+    //         validate: {
+    //             validator: function (value) {
+    //                 // expirationDate is required only if enabled is true
+    //                 return this.linkExpiration.enabled ? value != null : true;
+    //             },
+    //             message:
+    //                 "Expiration date is required when link expiration is enabled.",
+    //         },
+    //     },
+    // },
     shortUrl: {
         type: String,
         required: true,
         unique: true,
-        trim: true
     },
     urlCode: { 
         type: String, 
