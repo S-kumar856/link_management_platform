56e8364e8dcbf2d62677f6f171393b724831e761
S KUMAR <hk856856@gmail.com>
2025-01-27 01:29:05 +0530

    completed navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Register from './components/Register/Register'
 import Login from './components/Login/Login'
+import MainPage from './components/mainpage/MainPage'
+import Dashboard from './components/Dashboard/Dashboard'
+import LinkPage from './components/Dashboard/links'
+
 
 const App = () => {
   return (
@@ -9,9 +13,13 @@ const App = () => {
         <Routes>
             <Route path="/" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path='/mainpage' element={<MainPage/>}>
+              <Route path='dashboard' element={<Dashboard/>} />
+              <Route path='link' element={<LinkPage/>} />
+            </Route>
         </Routes>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Dashboard/links.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LinkPage = () => {
+  return (
+    <div>
+      im links page
+    </div>
+  )
+}
+
+export default LinkPage
